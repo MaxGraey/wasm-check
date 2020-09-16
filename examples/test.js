97e5099179328d0ccca625b62cd58c194ff1daae
MaxGraey <maxgraey@gmail.com>
2020-09-16 13:42:13 +0300

    internal refactoring. Add more tests

--- examples/test.js
+++ examples/test.js
@@ -1,5 +1,9 @@
+const assert = require('assert');
 const check = require('../lib/index');
+
+assert.strictEqual(check.support(), true);
+assert.notStrictEqual(check.support(2), true);
 
 console.log(check.feature.bulk);
 console.log(check.feature.simd);
-console.log(check.feature.mutableGlobals);
+console.log(check.feature.mutableGlobal);
